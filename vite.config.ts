8b871468b2bd0be2c99491668a65e66a585b502a
KuthitaRichard <richardkuthita1@gmail.com>
2025-06-06 14:47:42 +0300

    modified vite.config.ts from SPA to MPA on the build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "./",
   server: {
@@ -11,12 +10,20 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   build: {
-    outDir: "dist", // defines the folder to deploy for the hosting services
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about.html"),
+        services: path.resolve(__dirname, "services.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+        faq: path.resolve(__dirname, "faq.html"),
+      },
+    },
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
